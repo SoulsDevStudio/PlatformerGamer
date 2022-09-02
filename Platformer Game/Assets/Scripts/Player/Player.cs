a39b4e81b4168608d25507be6eb36cf25688e06d
Matheus Ferreira <soulsdevstudio@gmail.com>
2022-09-02 16:04:30 -0300

    Player
    Script
    Move

---
+++ Platformer Game/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rig;
+
+    public float speed;
+
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();   
+    }
+
+    void Move()
+    {
+        float movement = Input.GetAxis("Horizontal");
+
+        rig.velocity = new Vector2(movement * speed, rig.velocity.y);
+
+        if(movement > 0)
+        {
+            transform.eulerAngles = new Vector2(0,0);
+        }
+
+        if(movement < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+}
