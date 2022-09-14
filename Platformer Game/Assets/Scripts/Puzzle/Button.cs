7a185f4573162fa2095380b73a729211c1393a0c
Matheus Ferreira <soulsdevstudio@gmail.com>
2022-09-14 12:07:13 -0300

    Scene Build

--- Platformer Game/Assets/Scripts/Puzzle/Button.cs
+++ Platformer Game/Assets/Scripts/Puzzle/Button.cs
@@ -8,10 +8,20 @@ public class Button : MonoBehaviour
     Animator anim;
 
     public Animator animDoor;
+    public Transform point;
+
+    public float boxX;
+    public float boxY;
+    public float boxSize;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        BoxHit();
     }
 
     void OnPressed()
@@ -26,20 +36,23 @@ public class Button : MonoBehaviour
         animDoor.SetBool("Opening", false);
     }
 
-    void OnCollisionStay2D(Collision2D collision)
+    void BoxHit()
     {
-        if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("CubeStone"))
+        Collider2D hit = Physics2D.OverlapBox(point.position, new Vector2(boxX,boxY),boxSize);
+
+        if(hit != null)
         {
             OnPressed();
-        }    
-    }
-
-    void OnCollisionExit2D(Collision2D coll)
-    {
-        if (coll.gameObject.CompareTag("Player") || coll.gameObject.CompareTag("CubeStone"))
+        }
+        else
         {
             OnExit();
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(point.position, new Vector3(boxX, boxY, 0));
     }
 
 
